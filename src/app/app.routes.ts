029281b3882e23b92234843a99ea83aca4846af6
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2025-05-05 19:06:53 -0300

    Login Form updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,11 @@ import { PageNotFoundComponent } from './error-routing/not-found/not-found.compo
 import { UncaughtErrorComponent } from './error-routing/error/uncaught-error.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { LoginTravelAppComponent } from './login-travel-app/login-travel-app.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login-form', pathMatch: 'full' },
   { path: 'error', component: UncaughtErrorComponent },
   { path: 'login-form', component: LoginFormComponent, data: { text: 'Login-Form' } },
   { path: 'login-page', component: LoginPageComponent, data: { text: 'Login-Page' } },
-  { path: 'login-travel-app', component: LoginTravelAppComponent, data: { text: 'Login-Travel-App' } },
   { path: '**', component: PageNotFoundComponent } // must always be last
 ];
